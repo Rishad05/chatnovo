099c9ef38612b069a58731172c3ff1fb6dba8764
NSumaiya5 <sumaiyaaktar3029@gmail.com>
2021-05-29 23:34:52 +0600

    added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 import { useSelector } from 'react-redux';
-import { selectUser } from './features/counter/userSlice';
+import { login, selectUser } from './features/counter/userSlice';
+import Login from './Login';
 
 function App() {
   const user = useSelector(selectUser);
   return (
     <div className="App">
       {/* {user ? } */}
-
+<Login></Login>
  <h1>hii</h1>
     </div>
   );

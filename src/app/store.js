522e64aa2e301fad89a21d74ebe734155b472c7e
Rishad05 <iu.rishad5@gmail.com>
2021-05-30 12:00:32 +0600

    update

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-<<<<<<< HEAD
-import counterReducer from '../features/counter/userSlice';
-=======
 import userReducer from '../features/counter/counterSlice';
->>>>>>> 70eabfd9f1052d16dbd04b38bda48cf79d728128
 
 export const store = configureStore({
   reducer: {
